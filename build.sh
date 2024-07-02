df84fc0ebac1da9eced38e6f297ba28ca516d0fe
Esa-Matti Suuronen <esa-matti@suuronen.org>
2024-07-02 09:46:37 +0300

    cleanup

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-esbuild vendor-entries/preact.js --outdir=vendor --format=esm --minify --bundle
-esbuild vendor-entries/{htm.js,preact-hooks.js,preact-signals.js} --outdir=vendor --format=esm --minify --bundle --external:preact
+esbuild vendor/preact.js --outdir=vendor/build --format=esm --minify --bundle
+esbuild vendor/{htm.js,preact-hooks.js,preact-signals.js} --outdir=vendor/build --format=esm --minify --bundle --external:preact
--- vendor-entries/preact-hooks.js
+++
@@ -1,2 +0,0 @@
-// esbuild --external:preact ignores internal imports from preact/hooks so use this workaround
-export * from "../node_modules/preact/hooks/dist/hooks.mjs";
