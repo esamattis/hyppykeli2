a379ecdb25378c699527f47e9e78cd8d30155e3f
Esa-Matti Suuronen <github@esamatti.fi>
2024-07-27 12:42:53 +0300

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1152,7 +1152,7 @@ function Title() {
         <h1 id="title">
             <span class="title-name">${NAME}</span>
             <span class="title-temp" style=${{ opacity: historic ? 0.5 : 1 }}>
-                ${temperature?.toFixed(1)}°C maassa, ${temps?.[1].toFixed(1)}°C
+                ${temperature?.toFixed(1)}°C maassa, ${temps?.[4].toFixed(1)}°C
                 4km:ssä
                 ${h(
                     Help,
